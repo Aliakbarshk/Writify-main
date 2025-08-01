e12a7e3438af7a390fd2a603cafd8b54626da4f2
Aliakbarshk <aliakbarsk05@gmail.com>
2025-08-01 14:57:33 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,9 +4,9 @@ import {GoogleAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDqYGhSw07JQgjwnbOOceqWBDysphmFMwc",
-  authDomain: "writify-a823a.firebaseapp.com",
+  authDomain: "https://writfyyprototype1.netlify.app/",
   projectId: "writify-a823a",
-  storageBucket: "https://writfyyprototype1.netlify.app/",
+  storageBucket: "writify-a823a.firebasestorage.app",
   messagingSenderId: "588065529980",
   appId: "1:588065529980:web:7140d547d699f47e70e9f9",
   measurementId: "G-S4R49HN31R",
